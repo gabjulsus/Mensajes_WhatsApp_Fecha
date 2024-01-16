a449eb125369820c65186483c03deb37211be407
gabjulsus <gsus@adminprop.com>
2024-01-16 12:22:53 -0300

    avances

--- Mensajes_WhatsApp/Program.cs
+++ Mensajes_WhatsApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics.Contracts;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Data.SqlClient;
 
 namespace Mensajes_WhatsApp
 
@@ -46,7 +47,7 @@ namespace Mensajes_WhatsApp
             }
             sr2.Close();
 
-            Console.Write(Salida);
+           // Console.Write(Salida);
 
             string IDS = await u.JsIDAsync();
             IDS = IDS.Replace("\"", "").Replace("[", "").Replace("]", "");
@@ -56,11 +57,55 @@ namespace Mensajes_WhatsApp
             
             foreach (string id in ids) 
             { 
-               Console.WriteLine(id);
+                Console.WriteLine(id);
                 string JsonMsb=await u.JsMsgbyId(id);
                 sr3.WriteLine(id);
-                sr3.WriteLine(JsonMsb);
+                List<ChatMessage> chatMessages = JsonConvert.DeserializeObject<List<ChatMessage>>(JsonMsb);
+                string connectionString = "Data Source=127.0.0.1;Initial Catalog=AdminProp;Integrated Security=SSPI;";
+                SqlConnection connection = new SqlConnection(connectionString);
+                try
+                {
+                    connection.Open();
+
+                    foreach (var message in chatMessages)
+                    {
+
+                        DateTime fecha = DateTimeOffset.FromUnixTimeSeconds(message.Timestamp).UtcDateTime;
+                        DateTime fecha_local = fecha.ToLocalTime();
+                        string StrFecha = fecha_local.ToString("dd/MM/yyyy");
+                        DateTime fecha_insert = DateTime.Parse(StrFecha);
+
+                        SqlCommand command = new SqlCommand("InsertChatMessage", connection);
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                        // Configurar los parámetros para el procedimiento almacenado
+                        command.Parameters.AddWithValue("@Id", message.Id);
+                        command.Parameters.AddWithValue("@From", message.From);
+                        command.Parameters.AddWithValue("@To", message.To);
+                        command.Parameters.AddWithValue("@Author", message.Author);
+                        command.Parameters.AddWithValue("@Ack", message.Ack);
+                        command.Parameters.AddWithValue("@Body", message.Body);
+                        command.Parameters.AddWithValue("@FromMe", message.FromMe);
+                        command.Parameters.AddWithValue("@Type", message.Type);
+                        command.Parameters.AddWithValue("@Timestamp", fecha_insert);
+                        command.Parameters.AddWithValue("@IsForwarded", message.IsForwarded);
+                      
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                finally
+                {
+                    // Asegúrate de cerrar la conexión manualmente
+                    if (connection.State == System.Data.ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
             }
+
+
+
             sr3.Close();
         }
 
